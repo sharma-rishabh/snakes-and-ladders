dd3783eed4a4315497d25113518aed38372d73f8
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2025-02-21 14:53:53 +0530

    Add bg image to the board

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,21 @@
 import { useState } from "react";
 import { CoreSAL } from "./CoreSAL";
 import styles from "./styles.module.css";
+import bg from "./assets/board.jpg";
+
 
 const Game = ({ coreSAL }) => {
   const [board, setBoard] = useState(coreSAL.getBoard());
   const [diceValue, setDiceValue] = useState(1);
-  console.log(board);
   return (
     <div>
       <div
-        className={`grid grid-cols-10 gap-x-0 gap-y-0 bg-[url(/board.jpg)] ${styles.board}`}
+        className={`grid grid-cols-10 gap-x-0 gap-y-0] ${styles.board} -z-10`}
+        style={{ "background-image": `url(${bg.src})` }}
       >
         {board.map((cell, index) => {
           return (
-            <div
-              key={index}
-              id={index}
-              className={styles.cell}
-            >
+            <div key={index} id={index} className={styles.cell}>
               {cell.players.map((player, playerIndex) => {
                 return (
                   <div key={playerIndex} style={{ color: player.color }}>
