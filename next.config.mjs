98710d35c19d37c76b02963f627e5948d5cee94f
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2025-02-21 12:54:39 +0530

    Add gh pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/snakes-and-ladders/" : "",
+  basePath: isProd ? "/snakes-and-ladders" : "",
+  output: "export",
+};
 
 export default nextConfig;
