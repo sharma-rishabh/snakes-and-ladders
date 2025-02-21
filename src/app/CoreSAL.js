aee69e849f521031c86ac3b6c5804e9aef89f71c
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2025-02-21 15:10:37 +0530

    Added all porter details

--- src/app/CoreSAL.js
+++ src/app/CoreSAL.js
@@ -6,8 +6,18 @@ export class CoreSAL {
       { name: "Player 2", position: 0, color: "blue" },
     ];
     this.porters = [
-      // { start: 1, end: 3 },
-      // { start: 4, end: 2 },
+      { start: 3, end: 55 },
+      { start: 11, end: 49 },
+      { start: 13, end: 54 },
+      { start: 21, end: 57 },
+      { start: 27, end: 9 },
+      { start: 36, end: 2 },
+      { start: 40, end: 78 },
+      { start: 46, end: 15 },
+      { start: 53, end: 87 },
+      { start: 74, end: 31 },
+      { start: 93, end: 70 },
+      { start: 95, end: 41 },
     ];
   }
 
