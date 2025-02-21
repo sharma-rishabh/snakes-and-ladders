00a081650fbec43863b12cd38e88fc594e348a2d
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2025-02-21 12:17:44 +0530

    Add core game logic and basic UI

---
+++ src/app/CoreSAL.js
@@ -0,0 +1,72 @@
+export class CoreSAL {
+  constructor() {
+    this.currentPlayerIndex = 0;
+    this.players = [
+      { name: "Player 1", position: 0, color: "red" },
+      { name: "Player 2", position: 0, color: "blue" },
+    ];
+    this.porters = [
+      { start: 1, end: 3 },
+      { start: 4, end: 2 },
+    ];
+  }
+
+  getNewPosition(newPosition) {
+    for (const porter of this.porters) {
+      if (porter.start === newPosition) {
+        return porter.end;
+      }
+    }
+    return newPosition;
+  }
+
+  getNextPlayerIndex() {
+    return this.currentPlayerIndex === 0 ? 1 : 0;
+  }
+
+  updateCurrentPlayerIndex() {
+    this.currentPlayerIndex = this.getNextPlayerIndex();
+    return this.currentPlayerIndex;
+  }
+
+  movePlayer(diceValue) {
+    const currentPlayer = this.players[this.currentPlayerIndex];
+    const newPosition = currentPlayer.position + diceValue;
+    currentPlayer.position = this.getNewPosition(newPosition);
+    return this.players;
+  }
+
+  getBoard() {
+    const rows = 3;
+    const columns = 3;
+    const isEven = (num) => num % 2 === 0;
+    const getRow = (rowNumber) => {
+      return new Array(columns).fill(0).map((_, i) => {
+        const cellPosition = columns * rowNumber + i;
+        return {
+          cellPosition,
+          players: this.players.filter(
+            (player) => player.position === cellPosition
+          ),
+        };
+      });
+    };
+
+    return new Array(rows)
+      .fill(0)
+      .map((_, index) => {
+        return index;
+      })
+      .map((row) => {
+        return isEven(row) ? getRow(row) : getRow(row).reverse();
+      })
+      .reverse()
+      .flat();
+  }
+
+  playMove(diceValue) {
+    this.movePlayer(diceValue);
+    this.updateCurrentPlayerIndex();
+    return this.getBoard();
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,46 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { CoreSAL } from "./CoreSAL";
 
-export default function Home() {
+const Game = ({ coreSAL }) => {
+  const [board, setBoard] = useState(coreSAL.getBoard());
+  const [diceValue, setDiceValue] = useState(1);
+  console.log(board);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      className="grid grid-cols-3 gap-x-2 gap-y-0"
+      style={{ width: "150px" }}
+    >
+      {board.map((cell, index) => {
+        return (
+          <div
+            key={index}
+            style={{ border: "1px solid white", width: "50px", height: "50px" }}
+            id={index}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            {cell.players.map((player, playerIndex) => {
+              return (
+                <div key={playerIndex} style={{ color: player.color }}>
+                  {"O"}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+      <button
+        onClick={() => {
+          setDiceValue(1);
+          setBoard(coreSAL.playMove(diceValue));
+        }}
+      >
+        play
+      </button>
     </div>
   );
+};
+
+export default function Home() {
+  const coreSAL = new CoreSAL();
+  return <Game coreSAL={coreSAL} />;
 }
